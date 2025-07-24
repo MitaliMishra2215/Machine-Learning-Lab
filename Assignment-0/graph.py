e4aa9a336dd37ea72058d94349fbd45927d42e0c
Mitali255388 <mitalimishra9270@gmail.com>
2025-07-24 11:45:32 +0530

    Add files via upload

---
+++ Assignment-0/graph.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5]
+y=[2,4,6,8,10]
+
+plt.plot(x,y)
+plt.title("My First Graph of Machine Learning")
+plt.show()'''
+
+import matplotlib.pyplot as plt
+
+x=[1,2,25,89]
+y=[2,39,53,44]
+
+plt.plot(x,y)
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("My second Graph machine Learning")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
